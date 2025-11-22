535a1f427d205de0e694f131b5901f3768723118
CyberConductor <gilad4025@gmail.com>
2025-11-22 22:08:35 +0200

    added tracker to Dockerfile and telegram ssh alerter

--- sendtl.py
+++ sendtl.py
@@ -1,1 +1,22 @@
-import TelegramBot
+#!/usr/bin/env python3
+
+import requests
+import os
+from datetime import datetime
+
+TOKEN = "8317853350:AAHD_Hhh6MvkTK2iJVlQxDhVQY1yj2qcVl8"
+CHAT_ID = "7444335759"
+
+user = os.getenv("PAM_USER", "unknown")
+remote_host = os.getenv("PAM_RHOST", "unknown")
+time_now = datetime.now().strftime("%Y %m %d, %H:%M:%S")
+
+message = f"SSH login detected.\nUser: {user}\nFrom: {remote_host}\nTime: {time_now}"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+data = {"chat_id": CHAT_ID, "text": message}
+
+try:
+    requests.post(url, data=data)
+except Exception as e:
+    pass
