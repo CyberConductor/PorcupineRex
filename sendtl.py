4b503721820064aa9da49e840893b06d652ee16c
CyberConductor <gilad4025@gmail.com>
2025-12-06 21:35:53 +0200

    fixed telegram token

--- sendtl.py
+++ sendtl.py
@@ -23,11 +23,11 @@ remote = os.getenv("PAM_RHOST", "unknown")
 pam_type = os.getenv("PAM_TYPE", "")
 now = datetime.now().strftime("%Y %m %d, %H:%M:%S")
 
-# Successful login
+#
 if pam_type == "open_session":
     send(f"SSH login detected.\nUser: {user}\nFrom: {remote}\nTime: {now}")
 
-# Failed attempts come as pam_type == "auth"
+#failed 
 else:
     attempts +=1
 
