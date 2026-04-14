469ab2da02814f9d89e2d07eec4231cacbca47ed
CyberConductor <gilad4025@gmail.com>
2026-04-14 16:38:39 +0300

    remove file and replace with install auto script

---
+++ scripts/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install docker.io openssh-server -y
+sudo systemctl enable --now ssh
+sudo systemctl enable --now docker
+sudo usermod -aG docker $USER
+sudo docker build -t ubuntu_ho .
+sudo cp scripts/ssh_alert.py /usr/local/bin/ssh_alert.py
+sudo chmod +x /usr/local/bin/ssh_alert.py
+
+if ! grep -q "ssh_alert.py" /etc/pam.d/sshd; then
+    echo "session optional pam_exec.so /usr/bin/python3 /usr/local/bin/ssh_alert.py" | sudo tee -a /etc/pam.d/sshd
+fi
+
+cp scripts/vsftpd.conf /etc/vsftpd.conf
+sudo systemctl restart vsftpd
+
+
